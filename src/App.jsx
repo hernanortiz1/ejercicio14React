73b307fc624bee351dab9250e9e112c83dfc54eb
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-09 20:43:23 -0300

    agregue nav y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import Login from "./components/pages/Login"
-import Administrador from "./components/pages/Administrador"
-import Error404 from "./components/pages/Error404"
-import Inicio from "./components/pages/Inicio"
-import Footer from "./components/shared/Footer"
-import Menu from "./components/shared/Menu"
-import ProtectorAdmin from "./components/routes/ProtectorAdmin"
-import FormularioReceta from "./components/pages/recetas/FormularioReceta"
-import CardReceta from "./components/pages/recetas/CardReceta"
-import ItemReceta from "./components/pages/recetas/ItemReceta"
+import Login from "./components/pages/Login";
+import Administrador from "./components/pages/Administrador";
+import Error404 from "./components/pages/Error404";
+import Inicio from "./components/pages/Inicio";
+import Footer from "./components/shared/Footer";
+import Menu from "./components/shared/Menu";
+import ProtectorAdmin from "./components/routes/ProtectorAdmin";
+import FormularioReceta from "./components/pages/recetas/FormularioReceta";
+import CardReceta from "./components/pages/recetas/CardReceta";
+import ItemReceta from "./components/pages/recetas/ItemReceta";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Menu
-        
-        ></Menu>
+        <Menu></Menu>
         <main>
           <Routes>
             <Route path="/" element={<Inicio />}></Route>
-            
-            <Route
-              path="/login"
-              element={<Login></Login>}
-            ></Route>
+
+            <Route path="/login" element={<Login></Login>}></Route>
             <Route
               path="/administrador"
-              element={<ProtectorAdmin ></ProtectorAdmin>}
+              element={<ProtectorAdmin></ProtectorAdmin>}
             >
-             
-              <Route
-                index
-                element={
-                  <Administrador
-                   
-                  ></Administrador>
-                }
-              ></Route>
+              <Route index element={<Administrador></Administrador>}></Route>
 
               <Route
                 path="crear"
--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,11 +1,24 @@
-
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { NavLink, Link, useNavigate } from "react-router";
 
 const Menu = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to={"/"}>
+          <h2>Recetas</h2>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink className="nav-link" to={"/"}>
+              Inicio
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
