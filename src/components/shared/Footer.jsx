0270a651d85a3c70044d32a07daa89c63ee478b6
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-09 20:53:32 -0300

    agregue contenido a Login e imagenes

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-center bg-green text-light py-4">
+    <footer className="text-center bg-dark text-light py-4">
       <p>&copy; Todos los derechos reservados</p>
     </footer>
   );
