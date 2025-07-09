01a683717ef459063a3cd41e9257e4e75250579f
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-09 20:58:00 -0300

    agregue contenito a Inicio y una imagen al banner

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,11 +1,23 @@
-
+import { Container, Row } from "react-bootstrap";
+import CardReceta from "./recetas/CardReceta";
 
 const Inicio = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="mainSection">
+      <img
+        className="banner"
+        src="https://images.pexels.com/photos/1470166/pexels-photo-1470166.jpeg"
+        alt="Libro de recetas"
+      />
+      <Container className="mt-5">
+        <h1 className="display-4">Nuestras recetas</h1>
+        <hr />
+        <Row>
+          <CardReceta></CardReceta>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Inicio;
