122aa036f7f8c7a0a94a345998016479073340d0
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-09 21:33:47 -0300

    agregue funcion para usuarioLogueado y corregi rutas

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,50 @@ import ProtectorAdmin from "./components/routes/ProtectorAdmin";
 import FormularioReceta from "./components/pages/recetas/FormularioReceta";
 import CardReceta from "./components/pages/recetas/CardReceta";
 import ItemReceta from "./components/pages/recetas/ItemReceta";
+import { useEffect, useState } from "react";
 
 function App() {
+  const usuarioLogueado =
+    JSON.parse(sessionStorage.getItem("userKeyReceta")) || false;
+
+  const recetaLocalStorage =
+    JSON.parse(localStorage.getItem("catalogoRecetas")) || [];
+  const [usuarioAdmin, setUsuarioAdmin] = useState(usuarioLogueado);
+  const [recetas, setRecetas] = useState(recetaLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("catalogoRecetas", JSON.stringify(recetas));
+  }, [recetas]);
+  // se ejecuta en montaje
+
   return (
     <>
       <BrowserRouter>
-        <Menu></Menu>
+        <Menu
+          usuarioAdmin={usuarioAdmin}
+          setUsuarioAdmin={setUsuarioAdmin}
+        ></Menu>
         <main>
           <Routes>
             <Route path="/" element={<Inicio />}></Route>
 
-            <Route path="/login" element={<Login></Login>}></Route>
+            <Route
+              path="/login"
+              element={<Login setUsuarioAdmin={setUsuarioAdmin}></Login>}
+            ></Route>
             <Route
               path="/administrador"
-              element={<ProtectorAdmin></ProtectorAdmin>}
+              element={<ProtectorAdmin isAdmin={usuarioAdmin}></ProtectorAdmin>}
             >
-              <Route index element={<Administrador></Administrador>}></Route>
+              <Route
+                index
+                element={
+                  <Administrador
+                    setRecetas={setRecetas}
+                    recetas={recetas}
+                  ></Administrador>
+                }
+              ></Route>
 
               <Route
                 path="crear"
