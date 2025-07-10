382da8e6bfb12c2cfd90e8c7b2b5b3dd7b4889b7
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-09 21:01:43 -0300

    agrgue contenido a Error404 e imagen

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,10 +1,13 @@
-
+import error from "../../assets/robot_1984274.png";
 
 const Error404 = () => {
     return (
-        <div>
-            
-        </div>
+         <section className="mainSection text-center">
+              <img src={error} alt="error 404" />
+              <div>
+                <button className="btn btn-success" >Volver al inicio</button>
+              </div>
+            </section>
     );
 };
 
