b108b5cdd10f9b9dc28dcbe94a715b5459e7eeef
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-09 21:10:36 -0300

    agregue contenido a ItemReceta

--- src/components/pages/recetas/ItemReceta.jsx
+++ src/components/pages/recetas/ItemReceta.jsx
@@ -1,11 +1,29 @@
-
+import { Button } from "react-bootstrap";
 
 const ItemReceta = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <tr>
+      <td className="text-center">{/* fila */} </td> 
+      
+      <td> {/* nombreReceta */}</td>
+      <td className="text-center">
+        <img
+          src= ""//imagen 
+          className="img-thumbnail"
+          alt= "" //nombreReceta
+        ></img>
+      </td>
+      <td>{/* categoria */}</td>
+      <td className="text-center">
+        <Button variant="warning" className="me-lg-2">
+          <i className="bi bi-pencil-square"></i>
+        </Button>
+        <Button variant="danger">
+          <i className="bi bi-trash"></i>
+        </Button>
+      </td>
+    </tr>
+  );
 };
 
 export default ItemReceta;
