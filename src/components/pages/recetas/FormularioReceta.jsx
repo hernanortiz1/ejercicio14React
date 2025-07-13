baa10ed6e3015299a7a06a02225599ec228ff56f
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-13 14:04:26 -0300

    agregue sweetAlert y funcion para borrar recetas

--- src/components/pages/recetas/FormularioReceta.jsx
+++ src/components/pages/recetas/FormularioReceta.jsx
@@ -1,25 +1,35 @@
+import { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const FormularioReceta = () => {
   const navigate = useNavigate();
   const [receta, setReceta] = useState("");
+  const recetasLocalStorage =
+    JSON.parse(localStorage.getItem("listaRecetas")) || [];
+
+  const [recetas, setRecetas] = useState(recetasLocalStorage);
 
   const volverAdministrador = () => {
     navigate("/administrador");
   };
-
-  const recetasLocalStorage =
-    JSON.parse(localStorage.getItem("listaRecetas")) || [];
-  const [recetas, setRecetas] = useState(recetasLocalStorage);
 
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      nombreReceta: "",
+      imagen: "",
+      categoria: "",
+      descripcionBreve: "",
+      descripcionAmplia: "",
+    },
+  });
 
   useEffect(() => {
     console.log("desde use effect");
@@ -27,23 +37,67 @@ const FormularioReceta = () => {
     localStorage.setItem("listaRecetas", JSON.stringify(recetas));
   }, [recetas]);
 
- const agregarRecetas = (datos) => {
-    
-    setRecetas([...recetas, datos.inputReceta]);
+  const agregarRecetas = (datos) => {
+    Swal.fire({
+      title: "Datos guardados correctamente",
+      text: `Receta: ${datos.nombreReceta}, categoria: ${datos.categoria}`,
+      icon: "success",
+      draggable: true,
+    });
 
-    //limpiar formulario
+    setDatosCorrectos([...recetas, datos]);
+
     reset();
   };
 
+  const borrarRecetas = (recetaEliminada) => {
+    const indice = recetas.findIndex((item) => item === recetaEliminada);
+
+    if (indice !== -1) {
+      const datosNuevos = [...recetas];
+
+      datosNuevos.splice(indice, 1);
+      setRecetas(datosNuevos);
+    }
+  };
+
+  const borrarReceta = (nombreReceta) => {
+    const indice = tareas.findIndex((item) => item === nombreReceta);
+    //actualizar estado tareas
+    if (indice !== -1) {
+      //copio el array original
+      const nuevasRecetas = [...recetas];
+      //elimino con splice y actualizo
+      nuevasRecetas.splice(indice, 1);
+      setRecetas(nuevasRecetas);
+    }
+  };
 
   return (
     <section className="container mainSection">
       <h1 className="display-4 mt-5">Nueva receta</h1>
       <hr />
-      <Form className="my-4">
+      <Form className="my-4" onSubmit={handleSubmit(agregarRecetas)}>
         <Form.Group className="mb-3" controlId="formNombreReceta">
           <Form.Label>Receta*</Form.Label>
-          <Form.Control type="text" placeholder="Ej: Receta de..." />
+          <Form.Control
+            type="text"
+            placeholder="Ej: Receta de..."
+            onChange={(e) => setReceta(e.target.value)}
+            {...register("nombreReceta", {
+              required: "El titulo de la receta es un dato obligatorio",
+              minLength: {
+                value: 3,
+                message:
+                  "El titulo de la receta debe tener 3 caracteres como minimo ",
+              },
+              maxLength: {
+                value: 80,
+                message:
+                  "El titulo de la receta debe tener 50 caracteres como máximo",
+              },
+            })}
+          />
           <Form.Text className="text-danger"></Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
