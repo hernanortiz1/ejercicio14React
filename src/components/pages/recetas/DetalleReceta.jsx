4dd17d9a085c816a6a184900c574e2b0bc293265
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-13 21:33:58 -0300

    agregue shadow a card

--- src/components/pages/recetas/DetalleReceta.jsx
+++ src/components/pages/recetas/DetalleReceta.jsx
@@ -18,23 +18,27 @@ const DetalleReceta = ({ recetas }) => {
   }
 
   return (
-    <Container className="m-5">
+    <Container className="my-5">
       <Card className="rounded rounded-3 shadow">
-        <Card.Img variant="top" src={receta.imagen} alt={receta.nombreReceta}/>
+        <Card.Img variant="top" src={receta.imagen} alt={receta.nombreReceta} />
         <Card.Body>
-          <Card.Title className="text-center fs-1 border-bottom pb-3">{receta.nombreReceta}</Card.Title>
+          <Card.Title className="text-center fs-1 border-bottom pb-3">
+            {receta.nombreReceta}
+          </Card.Title>
           <Card.Text className="fs-5 border-3 shadow p-3 rounded-3 bg-primary-subtle">
             <strong>Categoría:</strong> {receta.categoria}
           </Card.Text>
           <Card.Text className="fs-5 border-3 shadow p-3 rounded-3 bg-primary-subtle">
             <strong>Descripción breve:</strong> {receta.descripcionBreve}
           </Card.Text>
-          <Card.Text className="fs-5 border-3 shadow p-3 rounded-3 bg-primary-subtle">
+          <Card.Text className="fs-5 border-3 shadow p-3 rounded-3 bg-primary-subtle mb-5">
             <strong>Descripción amplia:</strong> {receta.descripcionAmplia}
           </Card.Text>
-          <Button variant="secondary" onClick={() => navigate(-1)}>
-            Volver
-          </Button>
+          <div className="text-center d-grid gap-2 ">
+            <Button variant="primary" size="lg" onClick={() => navigate("/")}>
+              Volver
+            </Button>
+          </div>
         </Card.Body>
       </Card>
     </Container>
