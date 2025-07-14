d287e161c39890357d5577f925ca1122287816ce
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-13 21:41:33 -0300

    agregue alert para confirmar cerrar sesion

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,15 +1,34 @@
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { NavLink, Link, useNavigate } from "react-router";
+import Swal from "sweetalert2";
 
 const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
   const navegacion = useNavigate();
-  const logout = () => {
-    setUsuarioAdmin(false);
+  // const logout = () => {
+  //   setUsuarioAdmin(false);
 
-    sessionStorage.removeItem("userKeyReceta");
+  //   sessionStorage.removeItem("userKeyReceta");
 
-    navegacion("/");
+  //   navegacion("/");
+  // };
+
+   const logout = () => {
+    Swal.fire({
+      title: "¿Cerrar sesión?",
+      text: "¿Estás seguro de que deseas cerrar sesión?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sí, cerrar sesión",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        sessionStorage.removeItem("userKeyReceta");
+        setUsuarioAdmin(false);
+        navigate("/");
+      }
+    });
   };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
