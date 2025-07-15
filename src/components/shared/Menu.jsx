e23fd16b47f367bd20ae1113c851ff9e518d7d08
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-07-15 08:27:55 -0300

    agregue propiedad expanded para solucionar el bug de btn hamburguesa

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,16 +1,11 @@
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { NavLink, Link, useNavigate } from "react-router";
 import Swal from "sweetalert2";
+import { useState } from "react";
 
 const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
-  const navegacion = useNavigate();
-  // const logout = () => {
-  //   setUsuarioAdmin(false);
-
-  //   sessionStorage.removeItem("userKeyReceta");
-
-  //   navegacion("/");
-  // };
+  const navigate = useNavigate();
+  const [expanded, setExpanded] = useState(false);
 
   const logout = () => {
     Swal.fire({
@@ -27,24 +22,36 @@ const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
         navigate("/");
       }
     });
+    setExpanded(false); 
   };
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary" expanded={expanded}>
       <Container>
         <Navbar.Brand as={Link} to={"/"}>
           <h2>Recetas</h2>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink className="nav-link" to={"/"}>
+            <NavLink
+              className="nav-link"
+              to={"/"}
+              onClick={() => setExpanded(false)}
+            >
               Inicio
             </NavLink>
+
             {usuarioAdmin ? (
               <>
-                {/* si el usuario es admin renderiza el componente admin y un btn logout */}
-                <NavLink className="nav-link" to={"/administrador"}>
+                <NavLink
+                  className="nav-link"
+                  to={"/administrador"}
+                  onClick={() => setExpanded(false)}
+                >
                   Administrador
                 </NavLink>
                 <Button
@@ -56,14 +63,13 @@ const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
                 </Button>
               </>
             ) : (
-              <Button
-                as={NavLink}
-                to="/login"
-                variant="outline-primary"
-                className={({ isActive }) => (isActive ? "active" : "")}
+              <NavLink
+                className="nav-link"
+                to={"/login"}
+                onClick={() => setExpanded(false)}
               >
                 Login
-              </Button>
+              </NavLink>
             )}
           </Nav>
         </Navbar.Collapse>
